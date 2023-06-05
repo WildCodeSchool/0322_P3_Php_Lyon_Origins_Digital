cc099febac8f3f3e991f18f45a75cfe0dcaf66f7
val-fr69 <valentin.inacio@live.fr>
2023-06-05 17:40:43 +0200

    create VideoEntity

---
+++ src/Entity/Video.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VideoRepository::class)]
+class Video
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $post_date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $video_url = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $poster_url = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPostDate(): ?\DateTimeInterface
+    {
+        return $this->post_date;
+    }
+
+    public function setPostDate(\DateTimeInterface $post_date): self
+    {
+        $this->post_date = $post_date;
+
+        return $this;
+    }
+
+    public function getVideoUrl(): ?string
+    {
+        return $this->video_url;
+    }
+
+    public function setVideoUrl(string $video_url): self
+    {
+        $this->video_url = $video_url;
+
+        return $this;
+    }
+
+    public function getPosterUrl(): ?string
+    {
+        return $this->poster_url;
+    }
+
+    public function setPosterUrl(string $poster_url): self
+    {
+        $this->poster_url = $poster_url;
+
+        return $this;
+    }
+}
