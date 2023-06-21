e26b85ba898ba280c5b5e291f7dbf9b399bf3351
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-06-21 16:32:21 +0200

    add stats number and tabs to user dashboard

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
-    {
+    #[Route('/{id<\d+>}', name: 'dashboard')]
+    public function index(
+        User $user
+    ): Response {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $user,
         ]);
     }
 }
