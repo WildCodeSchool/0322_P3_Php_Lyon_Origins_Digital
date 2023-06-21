1a4e08c3940c36ccb79156507c9df81d877dd1ff
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-06-21 13:56:33 +0200

    removed commented code

--- assets/app.js
+++ assets/app.js
@@ -17,9 +17,9 @@ require('bootstrap');
 //boostrap css alert auto-close
 const alert = document.getElementById('alertMsg');
 //close the alert after 2 seconds (2000 milliseconds)
-// setTimeout(() => {
-//     alert.remove();
-// }, 2000);
+setTimeout(() => {
+    alert.remove();
+}, 2000);
 
 
 
