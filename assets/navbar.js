b696a9001a5df47b67d8653fc353fc03639ed354
maxdlr <contact@maxdlr.com>
2023-06-25 13:55:26 +0200

    doing navbar styling

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,6 +1,5 @@
 (function selectActiveNavbarItem(){
     const navGroup = document.getElementById('nav-group');
-
     const navItems = navGroup.getElementsByClassName('nav-item');
     const activePage = document.querySelector('main');
 
@@ -13,20 +12,34 @@
 
         if (
             activePage.id == navItem.getAttribute('aria-controls')
-            ) {
-                navItem.classList.add('text-secondary');
-                navItem.classList.remove('text-primary');
-                navItem.classList.toggle('bg-dark');
+        ) {
+            navItem.parentElement.classList.add('nav-item-link-active');
+            navItem.parentElement.classList.add('ps-3');
+            navItem.classList.add('nav-item-active');
+            navItem.classList.add('rounded-start-pill');
+            navItem.classList.add('text-secondary');
+            navItem.classList.remove('text-primary');
+            navItem.classList.toggle('bg-dark');
+
+            if (icon.classList.contains('bi-person-add')) {
+                icon.classList.remove('bi-person-add')
+                icon.classList.add('bi-person-fill-add')
+            } else {
                 icon.classList.remove('bi-'+iconId);
                 icon.classList.add('bi-'+iconId+'-fill');
-                navItem.setAttribute('aria-selected', 'true')
             }
 
-            if (navItem.getAttribute('aria-controls') == 'show' && navItem.getAttribute('aria-selected') == 'false') {
-                navItem.remove();
-            }
 
-            console.log(navItem.getAttribute('aria-selected'))
+            navItem.setAttribute('aria-selected', 'true')
+        }
+
+        if (activePage.id !== navItem.getAttribute('aria-controls')) {
+            navItem.parentElement.classList.add('nav-item-link-inactive');
+        }
+
+        if (navItem.getAttribute('aria-controls') == 'show' && navItem.getAttribute('aria-selected') == 'false') {
+            navItem.remove();
+        }
 
     }
 
