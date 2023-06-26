57a78b10296f557a18df135d7596ed07e8e8834c
maxdlr <contact@maxdlr.com>
2023-06-26 22:00:18 +0200

    add logout btn

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,18 +1,35 @@
+const activePage = document.querySelector('main');
+
+function fillIcons(icon, stroke, fill) {
+    if (icon.classList.contains('bi-'+stroke)) {
+        icon.classList.remove('bi-'+stroke)
+        icon.classList.add('bi-'+fill)
+    }
+}
+function emptyIcons(icon, stroke, fill) {
+    if (icon.classList.contains('bi-'+fill)) {
+        icon.classList.remove('bi-'+fill)
+        icon.classList.add('bi-'+stroke)
+    }
+}
+function removeWhenNotActive(conditionItem, removeItem = conditionItem) {
+    if(activePage.id !== conditionItem.getAttribute('aria-controls'))
+    {removeItem.parentElement.remove()}
+}
+
 function selectActiveNavbarItem(group){
     const navGroup = document.getElementById(group);
     const navItems = navGroup.getElementsByClassName('nav-item');
     const navItemShow = navGroup.querySelector('[aria-controls="show"]')
-    const activePage = document.querySelector('main');
+    const navItemUser = navGroup.querySelector('[aria-controls="user"]')
+    const navItemLogout = navGroup.querySelector('[aria-controls="logout"]')
 
-    if(navItemShow.getAttribute('aria-selected') == 'false' && activePage.id !== navItemShow.getAttribute('aria-controls'))
-    {navItemShow.parentElement.remove()}
-
+    removeWhenNotActive(navItemShow)
+    navItemUser == null ? navItemLogout.remove() : removeWhenNotActive(navItemUser, navItemLogout);
 
     for (const navItem of navItems) {
 
         const icon = navItem.getElementsByTagName('i')[0];
-        const iconId = icon.id;
-
 
         if (
             activePage.id == navItem.getAttribute('aria-controls')
@@ -32,16 +49,12 @@ function selectActiveNavbarItem(group){
                 navItem.classList.add('rounded-start-pill');
             }
 
-            if (icon.classList.contains('bi-person-add')) {
-                icon.classList.remove('bi-person-add')
-                icon.classList.add('bi-person-fill-add')
-            } else {
-                icon.classList.remove('bi-'+iconId);
-                icon.classList.add('bi-'+iconId+'-fill');
-            }
-
-            //to do: show logout only on /user/dashboard
-            //fix login icon on hover
+            fillIcons(icon,'house', 'house-fill')
+            fillIcons(icon,'person-check', 'person-fill-check')
+            fillIcons(icon,'person-up', 'person-fill-up')
+            fillIcons(icon,'person-slash', 'person-fill-slash')
+            fillIcons(icon,'play', 'play-fill')
+            fillIcons(icon,'gear', 'gear-fill')
 
             navItem.setAttribute('aria-selected', 'true')
         }
@@ -56,31 +69,26 @@ function selectActiveNavbarItem(group){
     for (const inactiveNavItem of inactiveNavItems) {
 
         const icon = inactiveNavItem.getElementsByTagName('i')[0];
-        const iconId = icon.id;
         inactiveNavItem.classList.add('z-1')
 
         inactiveNavItem.addEventListener('mouseover', function(){
-            if (icon.classList.contains('bi-person-add')) {
-                icon.classList.remove('bi-person-add')
-                icon.classList.add('bi-person-fill-add')
-            } else {
-                icon.classList.remove('bi-'+iconId);
-                icon.classList.add('bi-'+iconId+'-fill');
-            }
+            fillIcons(icon,'person-up', 'person-fill-up')
+            fillIcons(icon,'house', 'house-fill')
+            fillIcons(icon,'person-check', 'person-fill-check')
+            fillIcons(icon,'person-slash', 'person-fill-slash')
+            fillIcons(icon,'play', 'play-fill')
+            fillIcons(icon,'gear', 'gear-fill')
         })
+
         inactiveNavItem.addEventListener('mouseout', function(){
-            if (icon.classList.contains('bi-person-fill-add')) {
-                icon.classList.add('bi-person-add')
-                icon.classList.remove('bi-person-fill-add')
-            } else {
-                icon.classList.add('bi-'+iconId);
-                icon.classList.remove('bi-'+iconId+'-fill');
-            }
+            emptyIcons(icon,'person-up', 'person-fill-up')
+            emptyIcons(icon,'house', 'house-fill')
+            emptyIcons(icon,'person-check', 'person-fill-check')
+            emptyIcons(icon,'person-slash', 'person-fill-slash')
+            emptyIcons(icon,'play', 'play-fill')
+            emptyIcons(icon,'gear', 'gear-fill')
         })
     }
-
-
-
 }
 
 selectActiveNavbarItem('nav-group');
