e4bf3c9861f5424e7ce8954a08541d50972f613c
l00ma <looma@protonmail.ch>
2023-07-03 14:00:07 +0200

    fix useless uses

--- src/Controller/Admin/TagCrudController.php
+++ src/Controller/Admin/TagCrudController.php
@@ -3,13 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Tag;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 
 class TagCrudController extends AbstractCrudController
 {
