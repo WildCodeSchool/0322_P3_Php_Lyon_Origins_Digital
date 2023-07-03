3d7f86c3ccaddd8bd2764277321c76e127331a24
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-07-03 15:03:10 +0200

    improved style

--- assets/searchVideos.js
+++ assets/searchVideos.js
@@ -20,14 +20,14 @@ inputSearch.addEventListener('input', function(e){
                 const poster = require("/assets/images/" + video.posterUrl);
 
                 resultVideos.innerHTML +=
-                "<li class='rounded-4 my-2 py-3 bg-secondary border border-3 border-primary list-group-item'>"
+                "<li class='rounded-4 my-2 py-3 bg-primary border border-3 border-primary list-group-item'>"
                     + "<a class='link-underline link-underline-opacity-0' href='/video/show/"+ video.id + "'>"
                         + "<div class='row'>"
                             + `<img src="${poster}" class="col-6 img-fluid rounded-4"/>`
                             + "<div class='col-6 d-flex flex-column align-items-start justify-content-center'>"
-                                + "<h5 class='resultVideos-title title-font text-primary text-decoration-none'>" + video.title + "</h5>"
-                                + "<div class='resultVideos-tag tag-item bg-primary rounded-3 p-2 d-flex justify-content-center align-items-center m-1'>"
-                                    + "<h2 class='m-0 tag-list text-secondary'>#" + video.tag + "</h2>"
+                                + "<h5 class='resultVideos-title title-font text-secondary text-decoration-none'>" + video.title + "</h5>"
+                                + "<div class='resultVideos-tag tag-item bg-secondary rounded-3 p-2 d-flex justify-content-center align-items-center m-1'>"
+                                    + "<h2 class='m-0 tag-list text-primary'>#" + video.tag + "</h2>"
                                 + "</div>"
                             + "</div>"
                         + "</div>"
