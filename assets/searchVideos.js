4ed9a2a4d19a406f7dea0875bc738e28d193b69e
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-07-03 14:08:10 +0200

    fix bug and reset input on off focus and styling

--- assets/searchVideos.js
+++ assets/searchVideos.js
@@ -1,4 +1,5 @@
 const inputSearch = document.getElementById('searchVideos');
+const searchModal = document.getElementById('videoSearchModal');
 
 inputSearch.addEventListener('input', function(e){
     let search = e.target.value;
@@ -8,6 +9,7 @@ inputSearch.addEventListener('input', function(e){
         .then(videos => {
             const resultVideos = document.getElementById('resultVideos');
             const resultInfo = document.getElementById('resultInfo');
+            const checkUser = document.getElementById('modalUser');
 
             resultVideos.innerHTML = '';
 
@@ -15,17 +17,16 @@ inputSearch.addEventListener('input', function(e){
 
             for (const video of videos) {
 
-
                 const poster = require("/assets/images/" + video.posterUrl);
 
                 resultVideos.innerHTML +=
-                "<li class='rounded-4 my-2 py-3 bg-secondary border border-3 border-primary list-group-item list-group-item-action'>"
+                "<li class='rounded-4 my-2 py-3 bg-secondary border border-3 border-primary list-group-item'>"
                     + "<a class='link-underline link-underline-opacity-0' href='/video/show/"+ video.id + "'>"
                         + "<div class='row'>"
-                            + `<img src="${poster}" class="col-6 rounded-4"/>`
-                            + "<div class='col-6 d-flex flex-column align-items-start justify-content-center>'"
-                                + "<h5 class='title-font text-primary'>" + video.title + "</h5>"
-                                + "<div class='text-dark tag-item bg-primary rounded-3 p-2 d-flex justify-content-center align-items-center m-1'>"
+                            + `<img src="${poster}" class="col-6 img-fluid rounded-4"/>`
+                            + "<div class='col-6 d-flex flex-column align-items-start justify-content-center'>"
+                                + "<h5 class='resultVideos-title title-font text-primary text-decoration-none'>" + video.title + "</h5>"
+                                + "<div class='resultVideos-tag tag-item bg-primary rounded-3 p-2 d-flex justify-content-center align-items-center m-1'>"
                                     + "<h2 class='m-0 tag-list text-secondary'>#" + video.tag + "</h2>"
                                 + "</div>"
                             + "</div>"
@@ -34,5 +35,8 @@ inputSearch.addEventListener('input', function(e){
                 + "</li>"
             }
         })
+})
 
+searchModal.addEventListener('hidden.bs.modal', function(){
+    inputSearch.value = '';
 })
