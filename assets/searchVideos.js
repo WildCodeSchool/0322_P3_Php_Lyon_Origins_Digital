145753b9b3756d9ed5c7822c6e9294e457cb762f
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-07-04 16:26:53 +0200

    clean javascript

--- assets/searchVideos.js
+++ assets/searchVideos.js
@@ -1,29 +1,26 @@
 const inputSearch = document.getElementById('searchVideos');
 const searchModal = document.getElementById('videoSearchModal');
-// initié var de l'ul des resultats
 const resultVideos = document.getElementById('resultVideos');
 
 inputSearch.addEventListener('input', function(e){
     let search = e.target.value;
-    // vider les resutat
+
     resultVideos.innerHTML = '';
+    if (inputSearch.value.length >= 3) {
 
-    console.log(search)
+        fetch('/search-videos/'+ search)
+            .then(response => response.text())
+            .then(videos => {
+                resultVideos.innerHTML = '';
+                if (inputSearch.value.length >= 3) {
+                    let parser = new DOMParser();
+                    let html = parser.parseFromString(videos, 'text/html');
+                    let searchResult = html.body;
 
-    // if(search == '') checker si il y a plus de 3 char
-    if (search.length >= 3) {
-
-fetch('/search-videos/'+ search)
-.then(response => response.text())
-.then(videos => {
-
-                let parser = new DOMParser();
-                let html = parser.parseFromString(videos, 'text/html');
-                let searchResult = html.body;
-
-                resultVideos.innerHTML = searchResult.innerHTML;
+                    resultVideos.innerHTML = searchResult.innerHTML;
+                }
             })
-        }
+    }
 
     searchModal.addEventListener('hidden.bs.modal', function(){
         inputSearch.value = '';
