cf7614744d8a625a75d12fb3fec86531a59a22c8
val-fr69 <valentin.inacio@live.fr>
2023-07-06 12:39:47 +0200

    route on login page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,17 +7,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Exception as Exception;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(AuthenticationUtils $authenticationUtils, SessionInterface $session): Response
     {
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        $session->set('after_login', true);
+
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Repository\VideoRepository;
 use App\Repository\ViewedRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -16,7 +17,8 @@ class UserController extends AbstractController
     #[Route('/dashboard', name: 'dashboard')]
     public function index(
         ViewedRepository $viewedRepository,
-        VideoRepository $videoRepository
+        VideoRepository $videoRepository,
+        SessionInterface $session
     ): Response {
         $videoIdList = $viewedRepository->findVideosViewedByUser($this->getUser()->getId());
 
@@ -24,6 +26,13 @@ class UserController extends AbstractController
 
         foreach ($videoIdList as $videoId) {
             $history[] = $videoRepository->findOneBy(['id' => $videoId]);
+        }
+
+        if ($this->isGranted('ROLE_ADMIN') && $session->get('came_from_login')) {
+            // Reset the session variable
+            $session->set('after_login', false);
+
+            return $this->redirectToRoute('admin_dashboard');
         }
 
         return $this->render('user/index.html.twig', [
