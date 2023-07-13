0a6ee7f2cd4f56441a72c6a86193d72c3ee38296
maxdlr <contact@maxdlr.com>
2023-07-13 16:50:28 +0200

    removed confusing route

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,10 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/contact', name: 'contact_')]
 class ContactController extends AbstractController
 {
-    #[Route('', name: 'index')]
+    #[Route('/contact', name: 'contact_index')]
     public function index(
         Request $request,
         ContactRepository $contactRepository
