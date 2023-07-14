a4224cafefef693cadef9a47a0295c4cef97d170
l00ma <looma@protonmail.ch>
2023-07-14 19:12:13 +0200

    fix small bug in viewedFixtures

--- src/DataFixtures/ViewedFixtures.php
+++ src/DataFixtures/ViewedFixtures.php
@@ -14,11 +14,12 @@ class ViewedFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        $maxUsersValue = (count(UserFixtures::USERS)) - 1;
 
         $maxValue = (count(VideoFixtures::VIDEOS)) - 1;
         for ($i = 0; $i < 500; $i++) {
             $viewed = new Viewed();
-            $viewed->setUser($this->getReference('user_' . rand(0, 3)));
+            $viewed->setUser($this->getReference('user_' . rand(0, $maxUsersValue)));
             $viewed->setVideo($this->getReference('video_' . rand(0, $maxValue)));
             $dateTime = $faker->dateTimeThisDecade();
             $viewed->setViewDate(DateTimeImmutable::createFromMutable($dateTime));
